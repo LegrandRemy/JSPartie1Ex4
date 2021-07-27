00dd4059250bc2a3703177be5d41638fbc2986f8
LegrandRemy <legrandremy19@outlook.fr>
2021-07-27 15:55:52 +0200

    correction exercice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
+// Déclaration de la variable qui stock le bouton
 let sendformulaire1 = document.getElementById('sdformulaire');
+
+// 1ere facon d'appeler la fonction (fonction nominative)
 sendformulaire1.addEventListener('click', sendformulaire);
 
 function sendformulaire() {
@@ -8,3 +11,13 @@ function sendformulaire() {
     
     alert(`Nom : ${lastname1}\nPrénom : ${firstname1}\nVille : ${city1}`);
 }
+
+// ou 2ème facon d'appeler la fonction (fonction anonyme)
+
+// sendformulaire1.addEventListener('click', function() {
+//     let lastname1 = document.getElementById('name').value;
+//     let firstname1 = document.getElementById('firstname').value;
+//     let city1 = document.getElementById('city').value;
+    
+//     alert(`Nom : ${lastname1}\nPrénom : ${firstname1}\nVille : ${city1}`);
+// });
